5be4f07f7af6239046b63d840a1db87bd1c863db
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2022-07-28 08:05:56 +0200

    Create getAll() to retrieve array of food images

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  //costum function to return array of food images
+  getAll(): String[] {
+    return [
+      '/assets/images/foods/food-1.jpg',
+      '/assets/images/foods/food-2.jpg',
+      '/assets/images/foods/food-3.jpg',
+      '/assets/images/foods/food-4.jpg',
+      '/assets/images/foods/food-5.jpg',
+      '/assets/images/foods/food-6.jpg',
+    ]
+  }
+
 }
