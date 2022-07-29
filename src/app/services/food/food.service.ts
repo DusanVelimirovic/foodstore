fa22b1fe11b1209a066a325e69351cce64bea8d7
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2022-07-29 14:48:47 +0200

    Create food models

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food } from 'src/app/shared/models/Food';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,52 @@ export class FoodService {
   constructor() { }
 
   //costum function to return array of food images
-  getAll(): String[] {
+  getAll(): Food[] {
     return [
-      '/assets/images/foods/food-1.jpg',
-      '/assets/images/foods/food-2.jpg',
-      '/assets/images/foods/food-3.jpg',
-      '/assets/images/foods/food-4.jpg',
-      '/assets/images/foods/food-5.jpg',
-      '/assets/images/foods/food-6.jpg',
+      {
+        id: 1,
+        name: 'Pizza',
+        price: 100,
+        cookTime: '10-20',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-1.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Pasta',
+        price: 100,
+        cookTime: '10-20',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-2.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 3,
+        name: 'Burger',
+        price: 100,
+        cookTime: '10-20',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-3.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 4,
+        name: 'Pesto Sos',
+        price: 100,
+        cookTime: '10-20',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-4.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
     ]
   }
 
