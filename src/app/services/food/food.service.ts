b2f83a66621c6e8d30b5d1f413a3fbd4b93b3665
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2022-08-03 12:04:16 +0200

    Move search to home

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -8,6 +8,11 @@ export class FoodService {
 
   constructor() { }
 
+  getAllFoodsBySearchTerm(searchTerm: string): Food[] {
+    return this.getAll().filter(food =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
   getAllTags(): Tag[] {
     return [
       { name: 'All', count: 14 },
